b1b8ecdfd5083164afb963f7ea0088c3c6bcd7b9
Diogo Duarte <diogo.duarte@vnc.biz>
2015-10-12 18:40:58 +0100

    add website module

---
+++ addons/website_rem/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate Agency Website',
+    'version': '0.1',
+    'author': 'Diogo Duarte',
+    'summary': 'Real Estate Asset Management',
+    'description': """
+Public real estate agency website showroom
+==========================================
+This Module allows to sell/rent real estate units
+
+    """,
+    'website': 'https://diogocduarte.github.io',
+    'depends': ['rem', 'website'],
+    'category': 'Real Estate',
+    'demo': [
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ addons/website_rem/static/src/js/website_rem.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+$('.oe_website_rem').each(function () {
+    var oe_website_rem = this;
+
+});
+});
