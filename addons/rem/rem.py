a75e5ffeff95d38e923283c491da5e966a91186e
Diogo Duarte <diogo.duarte@vnc.biz>
2015-10-14 12:12:49 +0100

    added contract type

--- addons/rem/rem.py
+++ addons/rem/rem.py
@@ -14,7 +14,17 @@ class RemUniType(models.Model):
 
     name = fields.Char(string='Type Name', size=32, required=True, help="Type Name.")
     notes = fields.Text(string='Notes', help="Description of the type.")
-    active = fields.Boolean(string='Active', default=True, help="If the active field is set to False, it will allow you to hide the analytic journal without removing it.")
+    active = fields.Boolean(string='Active', default=True, help="If the active field is set to False, it will allow you to hide without removing it.")
+
+
+class RemUnitContractType(models.Model):
+    _name = 'contract.type'
+    _description = 'Contract Type'
+
+    name = fields.Char(string='Contract Name', size=32, required=True, help="Type of contract : renting, selling, selling ..")
+    sequence = fields.Integer(string='Sequence')
+    notes = fields.Text(string='Notes', help="Notes for the contract type.")
+    active = fields.Boolean(string='Active', default=True, help="If the active field is set to False, it will allow you to hide without removing it.")
 
 
 class RemUnitStage(models.Model):
@@ -25,16 +35,22 @@ class RemUnitStage(models.Model):
     sequence = fields.Integer(string='Sequence', help="Used to order stages. Lower is better.")
     notes = fields.Text(string='Notes', help="Description of the stage.")
     active = fields.Boolean(string='Active', default=True, help="If the active field is set to False, it will allow you to hide the analytic journal without removing it.")
-
+    contract_type_id = fields.Many2one('contract.type', string='Contract Type', required=False)
 
 class RemUnit(models.Model):
     _name = 'rem.unit'
     _description = 'Real Estate Unit'
 
+    @api.model
+    def _get_stage(self):
+        res = self.env['rem.unit.stage'].search([('contracttype_id', '=', False)], limit=1, order='sequence')
+        print res
+        return res
+
     name = fields.Char(string='Unit', size=32, required=True, help="Unit description (like house near riverside).")
     active = fields.Boolean(string='Active', default=True, help="If the active field is set to False, it will allow you to hide the analytic journal without removing it.")
     analytic_account_id = fields.Many2one('account.analytic.account', string='Contract/Analytic', help="Link this asset to an analytic account.")
-    stage_id = fields.Many2one('rem.unit.stage', string='Stage', select=True)
+    stage_id = fields.Many2one('rem.unit.stage', string='Stage', select=True, default=_get_stage)
     user_id = fields.Many2one('res.users', string='Salesman', required=False)
     bedrooms = fields.Integer(string='Number of bedrooms', default=1)
     bathrooms = fields.Integer(string='Number of bathrooms', default=1)
