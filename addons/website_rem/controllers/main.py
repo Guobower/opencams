0be0fdd1b56684b2e9986f9d36fa29d80d7c9562
Carlos Fonseca <carlosmigueldafonseca@gmail.com>
2015-10-19 15:57:58 +0100

    Adds pager

--- addons/website_rem/controllers/main.py
+++ addons/website_rem/controllers/main.py
@@ -10,28 +10,49 @@ from openerp.http import request
 from openerp.tools.translate import _
 from openerp.addons.website.models.website import slug
 
-PPG = 20 # Products Per Page
-PPR = 4  # Products Per Row
+PPG = 5 # Products Per Page
+PPR = 2  # Products Per Row
+
+class table_compute(object):
+    def __init__(self):
+        self.table = {}
 
 class website_rem(http.Controller):
 
     @http.route(['/rem', '/rem/page/<int:page>'], type='http', auth="public", website=True)
-    def rem(self, page=0, category=None, **post):
+    def rem(self, page=0, category=None, ppg=False, **post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        
+
+        if ppg:
+            try:
+                ppg = int(ppg)
+            except ValueError:
+                ppg = PPG
+            post["ppg"] = ppg
+        else:
+            ppg = PPG
+
         domain = []# later we will use for search
-        pager_offset = 0
+        pager_offset = PPR * (page - 1)
+        if pager_offset < 0: pager_offset = 0
+
+        url = "/rem"
+
         units_obj = pool.get('rem.unit')
         units_count = units_obj.search_count(cr, uid, domain, context=context)
-        unit_ids = units_obj.search(cr, uid, domain, limit=PPG, offset=pager_offset, context=context)
+        pager = request.website.pager(url=url, total=units_count, page=page, step=PPR, scope=7, url_args=post)
+        unit_ids = units_obj.search(cr, uid, domain, limit=PPR, offset=pager_offset, context=context)
         units = units_obj.browse(cr, uid, unit_ids, context=context)
+
         values = {
-            'units': units
+            'units': units,
+            'pager': pager
         }
 
-        RemUnits = http.request.env['rem.unit']
+        #RemUnits = http.request.env['rem.unit']
 
-        return http.request.render('website_rem.units', {
-            'rem_units': RemUnits.search([])
-        })
-    
+        #return http.request.render('website_rem.units', {
+        #    'rem_units': RemUnits.search([])
+        #})
+
+        return request.website.render("website_rem.units", values)
