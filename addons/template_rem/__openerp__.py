be5b9b7328e80c9b564a553b9f611e4b0586e2d9
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2016-05-20 16:43:15 +0100

    first change the homepage

---
+++ addons/template_rem/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate Agency Template',
+    'version': '0.1',
+    'author': 'Diogo Duarte',
+    'summary': 'Real Estate Asset Management',
+    'description': """
+Public real estate agency website temlate
+==========================================
+
+
+
+    """,
+    'website': 'http://www.odoogap.com',
+    'depends': ['web'],
+    'category': 'Real Estate',
+    'demo': [
+    ],
+    'data': [
+        'views/templates.xml'
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
