56621ab0a11f7e396999da5148d652426b11787e
eneves <eneves@odoogap.com>
2016-07-06 16:02:49 +0100

    [IMP] Added new Unit fields

--- addons/rem/models/crm.py
+++ addons/rem/models/crm.py
@@ -7,7 +7,10 @@ MATCH_RE = {
     're_type': 'type_id',
     're_rooms': 'rooms',
     're_bathrooms': 'bathrooms',
-    're_reason': 'reason'
+    're_reason': 'reason',
+    're_garages_spaces': 'garages',
+    're_city': 'city_id',
+    're_pool': 'sw_pool'
 }
 
 
@@ -16,18 +19,46 @@ class CrmLead(models.Model):
 
     unit_lead = fields.Many2many('rem.unit', string='Units')
 
+    # General Features
     re_contract_type_id = fields.Many2one(
         'contract.type', string='Contract Type')
     re_type = fields.Many2many('rem.unit.type', string='Property Type')
-    re_rooms = fields.Integer('Rooms', help="Number of rooms")
+    re_rooms = fields.Integer('Bedrooms', help="Number of rooms")
     re_bathrooms = fields.Integer(
         'Bathrooms', help="Number of bathrooms", re_field='bathrooms')
-    #re_datemovein = fields.Date('Deadline', help="Move in deadline for customer", default=lambda self: self._context.get('date', fields.Date.context_today(self)))
-    re_zone = fields.Char(
-        'Zone', help="place in order of gratest zone e.g. US, CA, Los Angeles, Beverly Hills")
+    re_city = fields.Char(
+        'City', help="place in order of gratest zone e.g. US, CA, Los Angeles, Beverly Hills")
     re_reason = fields.Many2one('reason.for.buy', string='Reason for Buy')
-    re_living_area = fields.Float('Living Area')
-    re_land_area = fields.Float('Land Area')
+    re_points_interest = fields.Many2many('location.preferences', string="Points of Interest")
+    
+
+    # Indoor Features
+    re_air_conditioned = fields.Boolean(
+        string="Air Conditioned", help="Active if you want to search for units with air conditioned.")
+    re_ducted_cooling = fields.Boolean(
+        string="Ducted Cooling", help="Active if you want to search for units with ducted cooling.")
+    re_wardrobes = fields.Boolean(
+        string="Built-in Wardrobes", help="Active if you want to search for units with built-in wardrobes.")
+    re_dishwasher = fields.Boolean(
+        string="Dishwasher", help="Active if you want to search for units with dishwasher.")
+    re_living_areas = fields.Integer(
+        'Living Areas', help="Number of living areas")
+
+    # Outdoor Features
+    re_backyard = fields.Boolean(
+        string="Backyard", help="Active if you want to search for units with backyard.")
+    re_dog_friendly = fields.Boolean(
+        string="Dog Friendly", help="Active if you want to search for units with dog friendly.")
+    re_garage_spaces = fields.Integer(
+        'Garage Spaces', help="Number of garage spaces")
+    re_secure_parking = fields.Boolean(
+        string="Secure Parking", help="Active if you want to search for units with Secure Parking.")
+    re_alarm = fields.Boolean(
+        string="Alarm System", help="Active if you want to search for units with alarm system.")
+    re_pool = fields.Boolean(
+        string="Swimming Pool", help="Active if you want to search for units with swimming pool.")
+    re_entertaining = fields.Boolean(
+        string="Outdoor Entertaining Area", help="Active if you want to search for units with outdoor entertaining area.")
 
     @api.multi
     def action_find_matching_units(self):
