d43e1df9de4f213d1152f1201f2a9d82e6e24e05
Carlos Fonseca <cfonseca@odoogap.com>
2016-07-13 19:34:42 +0100

    [REF] changes angularjs to html5 validations to reduce dependencies and to allow the creation of a contact form snippet

--- addons/theme_material/static/src/js/material.js
+++ addons/theme_material/static/src/js/material.js
@@ -1,26 +1,3 @@
-var app = angular.module('rem-website', [
-	'jcs-autoValidate',
-]);
-
-app.controller('rem-contact', function($scope) {
-
-	$scope.formModel = {};
-
-	$scope.onSubmit = function(valid) {
-		if (valid) {
-			var l = Ladda.create(document.querySelector("#rem-submit-button"));
-
-			l.start();
-
-			$('#rem_form input, #rem_form textarea').each(function() {
-				$("#submit-form-inputs").append("<input name=\"" + $(this).attr("name") + "\" value=\"" + $(this).val() + "\"/>");
-			});
-
-			$("#rem-hidden-form-submit").click();
-		}
-	};
-});
-
 $(function() {
 	"use strict";
 
