bd11ce89a39882d53c4b490d1bd5bfbbb26ee1cb
Carlos Fonseca <cfonseca@odoogap.com>
2016-07-13 19:16:30 +0100

    [FIX] contact menu

--- addons/website_rem/controllers/main.py
+++ addons/website_rem/controllers/main.py
@@ -9,16 +9,13 @@ PPG = 8  # Units Per Page
 
 class WebsiteContact(openerp.addons.web.controllers.main.Home):
 
-    @http.route(['/page/contactus'], type='http', auth='none')
-    def website_contact(self):
-        return werkzeug.utils.redirect('/contact-us', 303)
-
-
-class WebsiteRem(http.Controller):
-
     @http.route(['/contact-us'], type='http', auth='public', website=True)
     def website_rem_contact(self):
         return request.website.render('website_rem.contact_us_page')
+
+    @http.route(['/page/contactus'], type='http', auth='none')
+    def website_contact(self):
+        return werkzeug.utils.redirect('/contact-us', 303)
 
 
 class WebsiteRem(http.Controller):
