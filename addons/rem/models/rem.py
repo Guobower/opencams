8cb092b25c576bec4bf21d5fdf1aa7785c8d3b0b
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2016-07-25 20:48:28 +0100

    [FIX] website publish missing abstract method

--- addons/rem/models/rem.py
+++ addons/rem/models/rem.py
@@ -160,6 +160,12 @@ class RemUnit(models.Model):
     _name = 'rem.unit'
     _description = 'Real Estate Unit'
 
+    def _website_url(self, cr, uid, ids, field_name, arg, context=None):
+        res = super(RemUnit, self)._website_url(cr, uid, ids, field_name, arg, context=context)
+        for unit in self.browse(cr, uid, ids, context=context):
+            res[unit.id] = "/rem/unit/%s" % (unit.id,)
+        return res
+
     @api.model
     def _get_stage(self):
         return self.env['rem.unit.stage'].search([('contract_type_id', '=', False)], limit=1, order='sequence')
