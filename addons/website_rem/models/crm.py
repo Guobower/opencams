28824079cc3f16db1d70bcee616899c50d3d9430
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2016-07-25 16:20:38 +0100

    [FIX] fixed the seller form filter

--- addons/website_rem/models/crm.py
+++ addons/website_rem/models/crm.py
@@ -6,19 +6,24 @@ class Lead(models.Model):
 
     def website_form_input_filter(self, request, values):
         res = super(Lead, self).website_form_input_filter(request, values)
-        city = ''
-        if request.params.get('city'):
-            city = request.params.get('city').strip()
-        re_city = False
-        cities = self.sudo().env['rem.unit.city'].search_read(domain=[('name', '=ilike', (city or '') + "%")])
-        if len(cities) == 1:
-            re_city = cities[0]['id']
-        else:
-            values.update({'description': city})
-            
-        values.update({
-            'priority': '3',
-            'name': 'Seller contact: ' + request.params.get('name').strip(),
-            're_city': re_city,
-        })
+
+        if request.params.get('type_seller', False):
+
+            city = ''
+
+            if request.params.get('city'):
+                city = request.params.get('city').strip()
+            re_city = False
+            cities = self.sudo().env['rem.unit.city'].search_read(domain=[('name', '=ilike', (city or '') + "%")])
+            if len(cities) == 1:
+                re_city = cities[0]['id']
+            else:
+                values.update({'description': city})
+
+            values.update({
+                'priority': '3',
+                'name': 'Seller contact: ' + request.params.get('name').strip(),
+                're_city': re_city,
+            })
+
         return res
