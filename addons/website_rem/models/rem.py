7be285a19ee1c4cee0b87822f82e9540c6907e9b
Carlos Fonseca <cfonseca@odoogap.com>
2016-11-01 17:49:41 +0000

    [FIX] unit not found when trying to access unit page

--- addons/website_rem/models/rem.py
+++ addons/website_rem/models/rem.py
@@ -13,13 +13,3 @@ class RemUnit(models.Model):
         super(RemUnit, self)._compute_website_url()
         for unit in self:
             unit.website_url = "/rem/unit/%s" % (unit.id,)
-
-
-class RemUnitOfferType(models.Model):
-    _inherit = 'offer.type'
-
-    @api.multi
-    def _compute_website_url(self):
-        super(RemUnitOfferType, self)._compute_website_url()
-        for unit in self:
-            unit.website_url = "/rem?offer_type=%s" % (unit.id,)
