d533c5bace1e781b06174c4d6a283f20951eba83
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2018-02-12 09:27:58 +0000

    New version

--- rem_simplerets/models/connector.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import tools, api, fields, models, _
-
-
-class SimpleRetsConnector(models.Model):
-    _name = 're.connector.rets'
-    _inherit = 're.connector.abstract.model'
--- website_residential/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import crm
-from . import rem
-from . import website
